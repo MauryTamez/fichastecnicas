ccbd14b09c857b1441302faf7dc5116a1d86d32f
Enrique-OSM <liosauriopro11@outlook.com>
2026-07-10 11:11:59 -0600

    refacor: sincronizamos los prefijos admin/ para la vista de los recintos

--- frontend/src/api/venues.js
+++ frontend/src/api/venues.js
@@ -6,16 +6,16 @@ export const getVenues = async () => {
 };
 
 export const createVenue = async (venueData) => {
-    const response = await api.post('/venues', venueData);
+    const response = await api.post('/admin/venues', venueData);
     return response.data;
 };
 
 export const updateVenue = async (id, venueData) => {
-    const response = await api.put(`/venues/${id}`, venueData);
+    const response = await api.put(`/admin/venues/${id}`, venueData);
     return response.data;
 };
 
 export const deleteVenue = async (id) => {
-    const response = await api.delete(`/venues/${id}`);
+    const response = await api.delete(`/admin/venues/${id}`);
     return response.data;
 };
